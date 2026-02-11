80b32112cb18c858ff2612a247a1075540e3954d
Blackjack234 <paulanirban390@gmail.com>
2026-02-11 17:14:38 +0530

    initial commit

---
+++ src/modules/message/message.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MessageController } from './message.controller';
+import { MessageService } from './message.service';
+import { MessageRepository } from './repositories/message.repository';
+
+@Module({
+  controllers: [MessageController],
+  providers: [MessageService, MessageRepository],
+  exports: [MessageService, MessageRepository]
+})
+export class MessageModule {}
